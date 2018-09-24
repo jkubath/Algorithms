f4598073ecaecde876ad0a62ee2636e79a4c8d0e
Jonah Kubath <jkubath1@gmail.com>
2018-09-24 17:18:25 -0400

    A2 Data

--- JonahKubath_CS5310_A2/src/jonahKubath_a2/Main.java
+++ JonahKubath_CS5310_A2/src/jonahKubath_a2/Main.java
@@ -1,5 +1,6 @@
 package jonahKubath_a2;
 
+import java.util.Arrays;
 import java.util.Random;
 import sort.Sort;
 
@@ -7,87 +8,136 @@ public class Main {
 
 	public static void main(String[] args) {
 		int numberOfRuns = 5; // Run xTimes to calculate an average
-		int arrayLength = 10; // How many numbers to sort
+		int printLength = 10;
 		int randomNumbCeiling = 100; // Generate random numbers from 0 - 99
-		long time[] = new long[3]; // 0 = quicksort, 1 = mergesort, 2 = selection
+		long time[] = new long[4]; // 0 = quicksort, 1 = mergesort, 2 = selection
 		long start, end = 0; // Variables for run time
 		boolean firstRun = true; // Only print once and for the length of 10 numbers
-		
-		/* Generate the original array */
-		int data[] =new int[arrayLength];
-		
-		Random rand = new Random();
-		for(int i = 0; i < data.length; i++) {
-			data[i] = rand.nextInt(randomNumbCeiling);
-		}
-		
-		/* Run the sorts multiple times for an average time */
-		for(int i = 0; i < numberOfRuns; i++) {
-			/* Copy the original array so each sort is sorting
-			 * the same data
-			 */
-			int temp[] = new int[arrayLength];
-			
-			/* Quicksort */
-			temp = copyArray(data, temp);
-			if(firstRun && temp.length == 10) {
-				System.out.println("Quicksort");
-				System.out.print("Old - ");
-				printArray(temp);
-			}
-			start = end = 0; // Set time back to 0
-			start = System.nanoTime();
-			Sort.quickSort(temp, 0, temp.length - 1);
-			end = System.nanoTime();
-			time[0] += (end - start);
-			if(firstRun && temp.length == 10) {
-				System.out.print("New - ");
-				printArray(temp);
-			}
-			
-			/* Mergesort */
-			temp = copyArray(data, temp);
-			if(firstRun && temp.length == 10) {
-				System.out.println("Mergesort");
-				System.out.print("Old - ");
-				printArray(temp);
-			}
-			start = end = 0; // Set time back to 0
-			start = System.nanoTime();
-			Sort.mergeSort(temp, 0, temp.length - 1);
-			end = System.nanoTime();
-			time[1] += (end - start);
-			if(firstRun && temp.length == 10) {
-				System.out.print("New - ");
-				printArray(temp);
-			}
-			
-			/* selection sort */
-			temp = copyArray(data, temp);
-			if(firstRun && temp.length == 10) {
-				System.out.println("Selection sort");
-				System.out.print("Old - ");
-				printArray(temp);
-			}
-			start = end = 0; // Set time back to 0
-			start = System.nanoTime();
-			Sort.selection(temp);
-			end = System.nanoTime();
-			time[2] += (end - start);
-			if(firstRun && temp.length == 10) {
-				System.out.print("New - ");
-				printArray(temp);
-			}
-			
-			if(firstRun && temp.length == 10)
-				firstRun = false;
-		}
-		
-		//Print the time data
-		printAverageTime(time, numberOfRuns);
-		
-		
+		boolean verify = true; // Verify the sort algorithms with known methods
+		int testOffset = 1000;
+		int numberOfTests = 10 * testOffset;
 
+		
+		for(int j = 1000; j <= numberOfTests; j += testOffset) {
+			/* Generate the original array */
+			int data[] = new int[j];
+			Random rand = new Random();
+			for(int i = 0; i < data.length; i++) {
+				data[i] = rand.nextInt(randomNumbCeiling);
+			}
+			
+			time[0] = time[1] = time[2] = time[3] = 0; // Reset time
+			
+			/* Generate a sorted array for testing */
+			int sorted[] = new int[j];
+			if(verify) {
+				sorted = copyArray(data, sorted);
+				Arrays.sort(sorted);
+			}
+			
+			/* Run the sorts multiple times for an average time */
+			for(int i = 0; i < numberOfRuns; i++) {
+				/* Copy the original array so each sort is sorting
+				 * the same data
+				 */
+				int temp[] = new int[j];
+				
+				System.nanoTime();
+				
+				/* Quicksort */
+				temp = copyArray(data, temp);
+				if(firstRun && temp.length == printLength) {
+					System.out.println("Quicksort");
+					System.out.print("Old - ");
+					printArray(temp);
+				}
+				start = end = 0; // Set time back to 0
+				start = System.nanoTime();
+				Sort.quickSort(temp, 0, temp.length - 1);
+				end = System.nanoTime();
+				time[0] += (end - start);
+				if(firstRun && temp.length == printLength) {
+					System.out.print("New - ");
+					printArray(temp);
+				}
+				
+				if(verify) {
+					verifyArray(sorted, temp, "Quicksort");
+				}
+				
+				/* rQuickSort */
+				temp = copyArray(data, temp);
+				if(firstRun && temp.length == printLength) {
+					System.out.println("rQuickSort");
+					System.out.print("Old - ");
+					printArray(temp);
+				}
+				start = end = 0; // Set time back to 0
+				start = System.nanoTime();
+				Sort.rQuickSort(temp, 0, temp.length - 1);
+				end = System.nanoTime();
+				time[1] += (end - start);
+				if(firstRun && temp.length == printLength) {
+					System.out.print("New - ");
+					printArray(temp);
+				}
+				
+				if(verify) {
+					verifyArray(sorted, temp, "rQuicksort");
+				}
+				
+				/* Mergesort */
+				temp = copyArray(data, temp);
+				if(firstRun && temp.length == printLength) {
+					System.out.println("Mergesort");
+					System.out.print("Old - ");
+					printArray(temp);
+				}
+				start = end = 0; // Set time back to 0
+				start = System.nanoTime();
+				Sort.mergeSort(temp, 0, temp.length - 1);
+				end = System.nanoTime();
+				time[2] += (end - start);
+				if(firstRun && temp.length == printLength) {
+					System.out.print("New - ");
+					printArray(temp);
+				}
+				
+				if(verify) {
+					verifyArray(sorted, temp, "Mergesort");
+				}
+				
+				/* insertion sort */
+				temp = copyArray(data, temp);
+				if(firstRun && temp.length == printLength) {
+					System.out.println("Insertion sort");
+					System.out.print("Old - ");
+					printArray(temp);
+				}
+				start = end = 0; // Set time back to 0
+				start = System.nanoTime();
+				Sort.insertion(temp);
+				end = System.nanoTime();
+				time[3] += (end - start);
+				if(firstRun && temp.length == printLength) {
+					System.out.print("New - ");
+					printArray(temp);
+					System.out.println();
+				}
+				
+				if(verify) {
+					verifyArray(sorted, temp, "Insertion");
+				}
+				
+				if(firstRun && temp.length == printLength)
+					firstRun = false;
+			}
+			
+			//Print the time data
+			System.out.println("--------- " + j + " ---------");
+			printAverageTime(time, numberOfRuns);
+			
+		}
 	}
 	
 	public static int[] copyArray(int[] old, int[] copy) {
@@ -110,9 +160,29 @@ public class Main {
 	}
 	
 	public static void printAverageTime(long[] times, int runs) {
-		System.out.println("Quicksort - " + (times[0] / runs) + " ms");
-		System.out.println("Mergesort - " + (times[1] / runs) + " ms");
-		System.out.println("Selection - " + (times[2] / runs) + " ms");
+		System.out.println("Quicksort - " + (times[0] / runs / 1000) + " ms");
+		System.out.println("rQuickSort - " + (times[1] / runs / 1000) + " ms");
+		System.out.println("Mergesort - " + (times[2] / runs / 1000) + " ms");
+		System.out.println("Insertion - " + (times[3] / runs / 1000) + " ms");
+		System.out.println();
+	}
+	
+	public static void verifyArray(int sorted[], int test[], String name) {
+		boolean equal = true;
+		
+		if(sorted.length != test.length)
+			return;
+		
+		for(int i = 0; i < sorted.length; i++) {
+			if(sorted[i] != test[i]) {
+				equal = false;
+			}
+		}
+		
+		if(!equal) {
+			System.out.println("Error: " + name);
+			System.exit(1);
+		}
 	}
 
 }
--- JonahKubath_CS5310_A2/src/sort/Sort.java
+++ JonahKubath_CS5310_A2/src/sort/Sort.java
@@ -1,14 +1,17 @@
 package sort;
 
+import java.util.Random;
+
 public class Sort {
 	/*
 	 * Quicksort algorithm taken from: Computer Algorithms (Course Book)
+	 * rQuicksort algorithm taken from: Computer Algorithms (Course Book)
 	 * Mergesort algorithm taken from: https://gist.github.com/cocodrips/5937371
 	 */
 	public static int[] quickSort(int[] array,int left, int right) {
 		if(left < right) {
 			//Divide the problem into two parts
-			int partition = partition(array, left, right);
+			int partition = partition(array, left, right + 1);
 			
 			//Solve the sub problems
 			quickSort(array, left, partition - 1);
@@ -18,48 +21,75 @@ public class Sort {
 		return array;
 	}
 	
-	public static int partition(int[] array, int left, int right) {
-		int begin = left;
-		int pivot = array[left];
-		left++; // Start sorting with the value +1 of the pivot
+	public static int[] rQuickSort(int[] array,int left, int right) {
+		Random rand = new Random();
 		
-		while(left < right) {
-			//Iterate as long as the values are >
-			//than the pivot value
-			while(left < array.length && array[left] < pivot) {
-				left++;
+		if(left < right) {
+			if((right - left) > 5) {
+				int newLeft = (rand.nextInt(right - left) + left);
+				//System.out.println("NewLeft - " + newLeft);
+				swap(array, left, newLeft);
+				//printArray(array);
 			}
 			
-			while(right > 0 && array[right] > pivot) {
-				right--;
-			}
+			
+			//Divide the problem into two parts
+			int partition = partition(array, left, right + 1);
+			
+			//Solve the sub problems
+			rQuickSort(array, left, partition - 1);
+			rQuickSort(array, partition + 1, right);
+		}
+		
+		//printArray(array);
+		
+		return array;
+	}
+	
+	public static int partition(int[] array, int left, int right) {
+		//int begin = left;
+		int i = left;
+		int j = right;
+		int pivot = array[left];
+//		System.out.println("L- " + i + " R- " + j);
+//		System.out.println(pivot);
+		
+		do {
+			//Iterate as long as the values are >
+			//than the pivot value
+			do {
+				i++;
+			} while(i < array.length && array[i] < pivot);
+			
+			do {
+				j--;
+			} while(j > 0 && array[j] > pivot);
 			
 			/* Swap values so everything to the left
 			 * of the pivot is < pivot and everything
 			 * to the right is > pivot
 			 */
-			if(left < right)
-				swap(array, left, right);
+			if(i < j)
+				swap(array, i, j);
 			
-		}
+//			System.out.println("2L- " + i + " R- " + j);
+//			printArray(array);
+			
+		} while(i < j);
+//		System.out.println("3L- " + i + " R- " + j);
 		
-		array[begin] = array[right];
-		array[right] = pivot;
+		array[left] = array[j];
+		array[j] = pivot;
+
+//		printArray(array);
 		
-		return right;
+		return j;
 	}
 	
 	public static void swap(int[] array, int left, int right) {
 		int temp = array[left];
 		array[left] = array[right];
 		array[right] = temp;
-	}
-	
-	public static void printArray(int[] arr) {
-		for(int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + " ");
-		}
-		System.out.println("");
 	}
 	
 	public static void mergeSort(int[] array, int low, int high){
@@ -73,6 +103,7 @@ public class Sort {
 
 	public static void merge(int[] array, int low, int middle, int high){
 		int[] helper = new int[array.length];
+		
 		for (int i = low; i <= high; i++) {
 			helper[i] = array[i];
 		}
@@ -99,6 +130,44 @@ public class Sort {
 		}
 	}
 	
+	public static void merge1(int[] array, int low, int mid, int high) {
+		int b[] = new int[array.length];
+		int h = low;
+		int i = low;
+		int j = mid + 1;
+		
+		while((h <= mid) && (j <= high)) {
+			if(array[h] <= array[j]) {
+				b[i] = array[h];
+				h++;
+			}
+			else {
+				b[i] = array[j];
+				j++;
+			}
+			i++;
+		}
+		if( h > mid) {
+			for(int k = j; k <= high; k++) {
+				b[i] = array[k];
+				i++;
+			}
+		}
+		else {
+			for(int k = h; k <= mid; k++) {
+				b[i] = array[k];
+				i++;
+			}
+		}
+		
+		
+		for(int k = low; k <= high; k++) {
+			array[k] = b[k];
+		}
+		
+		
+	}
+	
 	public static void selection(int[] array) {
 		int smallest = 0;
 		
@@ -118,6 +187,30 @@ public class Sort {
 				array[smallest] = temp;
 			}
 		}
+	}
+	
+	public static void insertion(int[] array) {
+		
+		for(int i = 1; i < array.length; i++) {
+			int value = array[i];
+			int index = i - 1;
+			
+			while(index >= 0 && array[index] > value) {
+				array[index + 1] = array[index];
+				index--;
+			}
+			index++;
+			array[index] = value;
+		}
+		
+		
+	}
+	
+	public static void printArray(int[] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println("");
 	}
 	
 }
